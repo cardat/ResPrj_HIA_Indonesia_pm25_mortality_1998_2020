986c7646c1cb048f726934ad3866a6760c598ec9
cassandrayuen <yuen.cassandra.i@gmail.com>
2025-09-09 14:52:01 +1000

    Rejig main.R, add package installation instructions

--- main.R
+++ main.R
@@ -7,17 +7,14 @@
 # Load libraries and functions --------------------------------------------
 library(targets)
 
-# Run pipeline ------------------------------------------------------------
-## visualise targets
-tar_glimpse()
-tar_manifest() # or get a tibble
 
-## run pipeline
-tar_make()
+# Examine pipeline --------------------------------------------------------
+
+# See targets
+tar_manifest()
 
 # View pipeline status
 
-## visualise target status
 # click a target to highlight linked targets (use argument degree_to/degree_from to control number of edges to highlight, default 1)
 tar_visnetwork(targets_only = T) |>
   visNetwork::visHierarchicalLayout(direction = "UD", nodeSpacing = 200)
@@ -25,8 +22,13 @@ tar_visnetwork(targets_only = T) |>
 ## Alternative layout
 tar_visnetwork(targets_only = T) |>
   visNetwork::visIgraphLayout(layout = "layout_nicely", physics = T, smooth = T)
-  
 
+
+
+# Run pipeline ------------------------------------------------------------
+
+## run pipeline
+tar_make()
 
 
 # View target output ------------------------------------------------------
@@ -36,10 +38,16 @@ tar_visnetwork(targets_only = T) |>
 tar_read(data_attributable_number)
 
 ## view report
-browseURL("report.html")
+browseURL(tar_read(report)[1])
 
 
 # Debugging help ----------------------------------------------------------
 
+# show the warnings
 tar_meta(fields = warnings, complete_only = T)
+
+# or the errors
 tar_meta(fields = error, complete_only = T)
+
+# or show all target metadata
+tar_meta()

b596c76f3b62768e5ad6b8c11ea4cb47fdb3aaac
cassandrayuen <yuen.cassandra.i@gmail.com>
2025-09-09 10:09:02 +1000

    Fix all tmap plots to follow tmap v4 syntax (instead of v3)

--- R/04b0_viz_map_attributable_number.R
+++ R/04b0_viz_map_attributable_number.R
@@ -18,17 +18,20 @@ viz_map_attributable_number <- function(data_combine_exposure_response = data_co
   dat_an <- dat_an[, .(attributable = sum(attributable)), by = .(country_name, province, measure, age, cause, year)]
   v_an <- merge(v, dat_an[year %in% yy])
   
+  # plot
   tm_an <- tm_shape(v_an) +
-    tm_polygons(col = "attributable",
-                title = "Attributable number",
-                palette = "Reds") +
+    tm_polygons(fill = "attributable",
+                fill.legend = tm_legend(title = "Attributable number"),
+                fill.scale = tm_scale(values = "brewer.reds")) +
     tm_facets(by = "year", nrow = nrow_facet) + 
     tm_layout(asp = asp_facet) +
-    tm_scale_bar()
+    tm_scalebar()
   
-  tmap_save(tm_an, file.path(outdir, sprintf("map_attributable_%04i_%04i.png", min(yy), max(yy))))
+  # Save the plot
+  if(!dir.exists(outdir)) dir.create(outdir, recursive = T)
+  outfile <- file.path(outdir, sprintf("map_attributable_%04i_%04i.png", min(yy), max(yy)))
   
-  return(c(
-    file.path(outdir, sprintf("map_attributable_%04i_%04i.png", min(yy), max(yy)))
-  ))
+  tmap_save(tm_an, outfile)
+  
+  return(c(outfile))
 }
--- R/04b1_viz_map_attributable_number_alt.R
+++ R/04b1_viz_map_attributable_number_alt.R
@@ -1,11 +1,12 @@
 # targets::tar_workspace(fig_map_attributable_number_alt)
 
 viz_map_attributable_number_alt <- function(data_attributable_number_alt = data_attributable_number_alt,
-                                        file_tidy_geography = file_tidy_geography,
-                                        yy = 2015:2020,
-                                        outdir = outdirs$figs_tabs,
-                                        nrow_facet = floor(sqrt(length(yy))),
-                                        asp_facet = 6/4) {
+                                            file_tidy_geography = file_tidy_geography,
+                                            yy = 2015:2020,
+                                            outdir = outdirs$figs_tabs,
+                                            nrow_facet = floor(sqrt(length(yy))),
+                                            asp_facet = 6 / 4) {
+  
   # Read in data ####
   v <- sf::st_read(tar_read(file_tidy_geography), quiet = T)
   v <- sf::st_simplify(v, preserveTopology = T, dTolerance = 1000)
@@ -15,12 +16,12 @@ viz_map_attributable_number_alt <- function(data_attributable_number_alt = data_
   v_an <- merge(v, dat_an[year %in% yy])
   
   tm_an <- tm_shape(v_an) +
-    tm_polygons(col = "attributable",
-                title = "Attributable number",
-                palette = "Reds") +
+    tm_polygons(fill = "attributable",
+                fill.legend = tm_legend(title = "Attributable number"),
+                fill.scale = tm_scale(values = "brewer.reds")) +
     tm_facets(by = "year", nrow = nrow_facet) + 
     tm_layout(asp = asp_facet) +
-    tm_scale_bar()
+    tm_scalebar()
   
   tmap_save(tm_an, file.path(outdir, sprintf("map_attributable_calculated_%04i_%04i.png", min(yy), max(yy))))
   
